33756ddbdf62f108a6037b9717a55c0c6a9d857c
Princej258 <74393262+Princej258@users.noreply.github.com>
2022-08-11 17:08:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -15,11 +15,12 @@ def hangman():
     alphabet = set (string.ascii_uppercase)
     used_letters = set()    #what the user has guessed
     print("Let's play Hangman")
+    lives = 6
 
     #getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         #letters used
-        print("You have used these letters: ", " ".join(used_letters))
+        print("You have ", lives," lives left and you have used these letters: ", " ".join(used_letters))
 
         #what current word is (ie W - R D)
         word_list = [letter if letter in used_letters else '-' for letter in word]
@@ -29,11 +30,19 @@ def hangman():
             used_letters.add(user_letter)
             if user_letter in word_letters:
                 word_letters.remove(user_letter)
+            else:
+                lives = lives - 1
+                print("Letter is not in word.")
 
         elif user_letter in used_letters:
             print("You have already used that character. Please try again")
         
-        print("Invalid character. Please try again.")
-    print(f"Congrats, you won! The word was {word}.")
+        else:
+            print("Invalid character. Please try again.")
+    
+    if lives == 0:
+        print("You died, sorry. The word was ", word)
+    else:
+        print(f"Congrats, you won! The word was {word}.")
 
 print(hangman())
